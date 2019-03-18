d4604766271920117383f0a9e66605ed7eaf8650
tkcco <asd33tyu@gmail.com>
2019-03-18 11:21:54 +0900

    読み込むvueコンポーネントの変更

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 
 import '../scss/global.scss';
 
-import myApp from 'Components/test';
-// import myApp from 'Components/todo/';
+// import myApp from 'Components/test';
+import myApp from 'Components/todo/';
 // import myApp from 'Components/todo_router/Pages/Todos';
 
 new Vue({
