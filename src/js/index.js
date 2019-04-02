f0a23ac01ba080fd7062d9f8bd447d33a9dd56c3
tkcco <asd33tyu@gmail.com>
2019-04-02 15:07:23 +0900

    index.jsの変更

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 // import VueRouter from 'vue-router';
 
 // import routes from 'TodoRouterDir/routes';
-// import store from 'TodoRouterDir/store';
 
 import '../scss/global.scss';
 
@@ -25,5 +24,4 @@ new Vue({
   // }
 
   // router,
-  // store,
 });
