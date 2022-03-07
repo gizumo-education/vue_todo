ff0bd881d47894b825cb26774a52d8225110020f
Admin 水野 <mizno911.zeta@gmail.com>
2022-03-07 09:09:12 +0900

    importをvuexに切り替え

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,30 @@
 import Vue from 'vue';
-// import VueRouter from 'vue-router';
+import VueRouter from 'vue-router';
 
 // import routes from 'TodoRouterDir/routes';
-// import routes from 'TodoVuexDir/routes';
-// import store from 'TodoVuexDir/store';
+import routes from 'TodoVuexDir/routes';
+import store from 'TodoVuexDir/store';
 // import routes from 'VuexSample/routes';
 // import store from 'VuexSample/store';
 
 import '../scss/global.scss';
 
-import myApp from './first';
+// import myApp from './first';
 // import myApp from 'TodoDir';
 // import myApp from 'TodoRouterDir';
-// import myApp from 'TodoVuexDir';
+import myApp from 'TodoVuexDir';
 // import myApp from 'VuexSample';
 
-// Vue.use(VueRouter);
-// const router = new VueRouter({
-//   routes,
-//   mode: 'history',
-// });
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+});
 
 new Vue({
   el: '#app',
-  // router,
-  // store,
+  router,
+  store,
   render: h => h(myApp),
   // render: h => h(myApp), は↓の書き方を短くしたもの
   // render: function (createElement) {
