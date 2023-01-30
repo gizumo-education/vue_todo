2ed37fa9bbd91153f1f05ecb677442b17a44f676
Ryo Kihara <work1008.rk@gmail.com>
2023-01-30 13:03:41 +0900

    refactor: セミコロン

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue2'
+import vue from '@vitejs/plugin-vue2';
 import legacy from '@vitejs/plugin-legacy';
 import checker from 'vite-plugin-checker';
 
